0424e02d7e4d011ed47d9758cb3575b03c00a18d
brian99na <brian99na@gmail.com>
2021-12-17 23:13:33 -0800

    added create post button on the bottom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Homepage from './components/Homepage/Homepage';
 import { AiOutlinePlus } from 'react-icons/ai'
 
 function App() {
+
+  const navigate = useNavigate()
+
+  const handleCreateClick = () => {
+    setTimeout(() => {
+      navigate('/create')
+    }, 500)
+  }
+
   return (
     <div className="App">
       <Header />
@@ -14,8 +23,8 @@ function App() {
       <div className='create-icon'>
         <div className='create-icon-main'>
           <div className='create-icon-1'></div>
-          <AiOutlinePlus className='create-icon-2'/>
-          <div className='create-icon-2'></div>
+          <AiOutlinePlus className='create-icon-2' onClick={handleCreateClick}/>
+          <div className='create-icon-3'></div>
         </div>
       </div>
     </div>

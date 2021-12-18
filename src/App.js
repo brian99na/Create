a8d567080151b48bea45948fb272c4f068a2f65e
brian99na <brian99na@gmail.com>
2021-12-17 20:53:17 -0800

    added styling and all required tags for nav, need extra styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Homepage from './components/Homepage/Homepage';
+import { AiOutlinePlus } from 'react-icons/ai'
 
 function App() {
   return (
@@ -10,6 +11,11 @@ function App() {
       <Routes>
         <Route path='/' exact element={() => {<Homepage />}}/>
       </Routes>
+      <div className='create-icon-1'>
+        <div className=''></div>
+        <AiOutlinePlus />
+        <div className='create-icon-2'></div>
+      </div>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RiSearch2Line } from 'react-icons/ri'
+import { Link, useNavigate } from 'react-router-dom'
+import './header.css'
 
-function Header() {
+function Header(props) {
+
+    const [navActive, setNavActive] = useState(false)
+    const [searchActive, setSearchActive] = useState(false)
+    const navigate = useNavigate()
+
+    const handleNavClick = () => {
+        setNavActive(!navActive)
+    }
+
+    const handleSearchClick = () => {
+        setSearchActive(!searchActive)
+    }
+
+    const handleDelayClick = () => {
+        setTimeout(() => {
+            navigate('/')
+        }, 500)
+    }
+
     return (
         <header>
             <nav>
-                <div className='nav-icon'></div>
-                <div className='create-btn'>[CREATE]</div>
-                <div className='search-box'>
+                <div className='create-btn' onClick={handleDelayClick}>
+                    <div className='create-1'></div>
+                    <h1>CREATE</h1>
+                    <div className='create-2'></div>
+                </div>
+                <div className={`search-box ${searchActive ? 'search-active' : ''}`}>
                     <input placeholder='[search]'/>
                 </div>
                 <div className='search-ham'>
-                    <RiSearch2Line />
-                    <div className='ham-1'></div>
-                    <div className='ham-2'></div>
-                    <div className='ham-3'></div>
+                    <RiSearch2Line className='search-icon' onClick={handleSearchClick}/>
+                    <div className='ham-main' onClick={handleNavClick}>
+                        <div className='ham-1'></div>
+                        <div className='ham-2'></div>
+                        <div className='ham-3'></div>
+                    </div>
                 </div>
             </nav>
-            <div className='nav-active'>
-                <h1>[Sign In]</h1>
-                <h1>[Home]</h1>
-                <h1>[Profile]</h1>
-                <h1>[Create]</h1>
+            <div className={`nav-links ${navActive ? 'nav-links-active' : ''}`}>
+                <Link to='/sign-in'>
+                    <h1>[Sign In]</h1>
+                </Link>
+                <Link to='/'>
+                    <h1>[Home]</h1>
+                </Link>
+                <Link to={`/${props.username}`}>
+                    <h1>[Profile]</h1>
+                </Link>
             </div>
         </header>
     )

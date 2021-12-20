331203bf29be89a1e24e0e226feef261b716222f
brian99na <brian99na@gmail.com>
2021-12-19 20:23:26 -0800

    finished sliding carousel basics for create form

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -9,6 +9,7 @@ import './create.css'
 function Create(props) {
     const [formData, setFormData] = useState({file: {title: '', file: ''}, title: '', desc: '', tags: []})
     const [tagValue, setTagValue] = useState('')
+    const [slideActive, setSlideActive] = useState(1)
     const navigate = useNavigate()
 
     const handleChange = (e) => {
@@ -25,47 +26,66 @@ function Create(props) {
     const handleTagSubmit = (e) => {
         e.preventDefault();
         let tagArr = [...formData.tags]
-        tagArr.append(tagValue)
+        tagArr.push(tagValue)
         setFormData({...formData, tags: tagArr})
+        setTagValue('')
     }
 
     const handleTagChange = (e) => {
         setTagValue(e.target.value)
     }
 
-    const removeTagClick = (e) => {
+    const removeTagClick = (e, index) => {
         let tagArr = [...formData.tags]
-        tagArr.splice(e.target.value, 1)
+        tagArr.splice(index, 1)
+        setFormData({...formData, tags: tagArr})
+    }
+
+    const handleSlide = (e) => {
+        if (e.target.name === 'next' & slideActive < 3) {
+            setSlideActive(slideActive + 1)
+        } else if (e.target.name === 'back' & slideActive > 1) {
+            setSlideActive(slideActive - 1)
+        }
     }
 
     const tagsJsx = formData.tags && formData.tags.map((tag, index) => {
         return(
             <div className='tag-container'>
                 <p className='tag'>{tag}</p>
-                <CgRemove value={index} onClick={removeTagClick}/>
+                <CgRemove className='remove-tag' onClick={(e) => removeTagClick(e, index)}/>
             </div>
         )
     })
 
     return (
         <div className='create-modal'>
-            <form className='create-form'>
-                <div className='upload-container'>
-                    <input type='file' hidden/>
-                    <button>
-                        Upload File
-                    </button>
+            <div className={`create-form ${slideActive === 1 ? 'form-left' : slideActive === 2 ? 'form-mid' : slideActive === 3 ? 'form-right' : ''}`}>
+                <div className='upload-slide'>
+                    <div className='skeleton-load'></div>
+                    <div className='upload-container'>
+                        <input type='file' hidden/>
+                        <button>[upload]</button>
+                    </div>
                 </div>
-                <div className='title-desc'>
-                    <input placeholder='Title' name='title' value={formData.title} onChange={handleChange}/>
-                    <textarea placeholder='Description' name='desc' value={formData.desc} onChange={handleChange}/>
+                <div className='title-desc-slide'>
+                    <div className='title-desc'>
+                        <input placeholder='title' maxLength='24' name='title' value={formData.title} onChange={handleChange}/>
+                        <textarea placeholder='description' name='desc' value={formData.desc} onChange={handleChange}/>
+                    </div>
                 </div>
-                <form className='tags' onSubmit={handleTagSubmit}>
+                <form className='tags-slide' onSubmit={handleTagSubmit}>
+                    <div className='tags-container'>
+                        {tagsJsx}
+                    </div>
                     <input placeholder='input tags' value={tagValue} onChange={handleTagChange}/>
-                    {tagsJsx}
                 </form>
-                <button type='submit'>[create]</button>
-            </form>
+            </div>
+            <div className='btn-container'>
+                <button name='back' onClick={handleSlide} className={`back-btn ${slideActive >= 2 ? 'btn-active' : ''}`}>[back]</button>
+                <button name='next' onClick={handleSlide} className={`next-btn ${slideActive <= 2 ? 'btn-active' : ''}`}>[next]</button>
+                <button className={`create-btn ${slideActive === 3 ? 'btn-active' : ''}`}>[create]</button>
+            </div>
             <div className='create-icon'>
                 <div className='create-icon-1'></div>
                 <AiOutlinePlus className='create-icon-2' onClick={handleCreateClick}/>

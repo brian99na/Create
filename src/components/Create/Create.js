7030bc20e1a07b0344b80f1b94f7fd677fce63c0
brian99na <brian99na@gmail.com>
2021-12-19 23:17:39 -0800

    fully functioning create form, needs post req to backend

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,16 +1,15 @@
 import React, { useRef, useState } from 'react'
 import { AiOutlinePlus } from 'react-icons/ai'
 import { CgRemove } from 'react-icons/cg'
-import { useNavigate } from 'react-router'
 import './create.css'
 
 
 
 function Create(props) {
-    const [formData, setFormData] = useState({file: {title: '', file: ''}, title: '', desc: '', tags: []})
+    const [createActive, setCreateActive] = useState(false)
+    const [formData, setFormData] = useState({file: {title: '[upload]', file: ''}, title: '', desc: '', tags: ['art']})
     const [tagValue, setTagValue] = useState('')
     const [slideActive, setSlideActive] = useState(1)
-    const navigate = useNavigate()
     const fileRef = useRef()
 
     const handleChange = (e) => {
@@ -18,10 +17,7 @@ function Create(props) {
     }
 
     const handleCreateClick = () => {
-        props.setPageLeave(false)
-        setTimeout(() => {
-          navigate('/create')
-        }, 500)
+        setCreateActive(!createActive)
     }
 
     const handleFileUpload = () => {
@@ -30,8 +26,7 @@ function Create(props) {
 
     const handleFileChange = () => {
         console.log(fileRef.current.files[0])
-        setFormData({...formData, file: {title: fileRef.current.files[0].name, file: fileRef.current.files[0]}})
-
+        setFormData({...formData, file: {title: fileRef.current.files[0].name, file: URL.createObjectURL(fileRef.current.files[0])}})
     }
 
     const handleTagSubmit = (e) => {
@@ -60,6 +55,13 @@ function Create(props) {
         }
     }
 
+    const handleCreate = () => {
+        setCreateActive(false)
+        setSlideActive(1)
+        // Axios call - upload files then do this \/
+        setFormData({file: {title: '[upload]', file: ''}, title: '', desc: '', tags: []})
+    }
+
     const tagsJsx = formData.tags && formData.tags.map((tag, index) => {
         return(
             <div className='tag-container'>
@@ -70,37 +72,41 @@ function Create(props) {
     })
 
     return (
-        <div className='create-modal'>
-            <div className='slide-indicators'>
-                <div className={`circle ${slideActive === 1 ? 'dot' : ''}`}></div>
-                <div className={`circle ${slideActive === 2 ? 'dot' : ''}`}></div>
-                <div className={`circle ${slideActive === 3 ? 'dot' : ''}`}></div>
-            </div>
-            <div className={`create-form ${slideActive === 1 ? 'form-left' : slideActive === 2 ? 'form-mid' : slideActive === 3 ? 'form-right' : ''}`}>
-                <div className='upload-slide'>
-                    <div className='skeleton-load'></div>
-                    <div className='upload-container'>
-                        <input ref={fileRef} onChange={handleFileChange} type='file' hidden/>
-                        <button onClick={handleFileUpload}>[upload]</button>
-                    </div>
+        <div className='modal-upper'>
+            <div className={`create-modal ${createActive ? 'modal-active' : ''}`}>
+                <div className='slide-indicators'>
+                    <div className={`circle ${slideActive === 1 ? 'dot' : ''}`}></div>
+                    <div className={`circle ${slideActive === 2 ? 'dot' : ''}`}></div>
+                    <div className={`circle ${slideActive === 3 ? 'dot' : ''}`}></div>
                 </div>
-                <div className='title-desc-slide'>
-                    <div className='title-desc'>
-                        <input placeholder='title' maxLength='24' name='title' value={formData.title} onChange={handleChange}/>
-                        <textarea placeholder='description' name='desc' value={formData.desc} onChange={handleChange}/>
+                <div className={`create-form ${slideActive === 1 ? 'form-left' : slideActive === 2 ? 'form-mid' : slideActive === 3 ? 'form-right' : ''}`}>
+                    <div className='upload-slide'>
+                        <div className={`skeleton-load ${formData.file.file ? 'skeleton-invis' : ''}`}>
+                            <img src={formData.file.file} alt=''/>
+                        </div>
+                        <div className='upload-container'>
+                            <input ref={fileRef} onChange={handleFileChange} type='file' hidden/>
+                            <button onClick={handleFileUpload}>{formData.file.title}</button>
+                        </div>
                     </div>
+                    <div className='title-desc-slide'>
+                        <div className='title-desc'>
+                            <input placeholder='title' maxLength='24' name='title' value={formData.title} onChange={handleChange}/>
+                            <textarea placeholder='description' name='desc' value={formData.desc} onChange={handleChange}/>
+                        </div>
+                    </div>
+                    <form className='tags-slide' onSubmit={handleTagSubmit}>
+                        <div className='tags-container'>
+                            {tagsJsx}
+                        </div>
+                        <input placeholder='input tags' value={tagValue} onChange={handleTagChange}/>
+                    </form>
                 </div>
-                <form className='tags-slide' onSubmit={handleTagSubmit}>
-                    <div className='tags-container'>
-                        {tagsJsx}
-                    </div>
-                    <input placeholder='input tags' value={tagValue} onChange={handleTagChange}/>
-                </form>
-            </div>
-            <div className='btn-container'>
-                <button name='back' onClick={handleSlide} className={`back-btn ${slideActive >= 2 ? 'btn-active' : ''}`}>[back]</button>
-                <button name='next' onClick={handleSlide} className={`next-btn ${slideActive <= 2 ? 'btn-active' : ''}`}>[next]</button>
-                <button className={`create-btn ${slideActive === 3 ? 'btn-active' : ''}`}>[create]</button>
+                <div className='btn-container'>
+                    <button name='back' onClick={handleSlide} className={`back-btn ${slideActive >= 2 ? 'btn-active' : ''}`}>[back]</button>
+                    <button name='next' onClick={handleSlide} className={`next-btn ${slideActive <= 2 ? 'btn-active' : ''}`}>[next]</button>
+                    <button onClick={handleCreate} className={`create-btn ${slideActive === 3 ? 'btn-active' : ''}`}>[create]</button>
+                </div>
             </div>
             <div className='create-icon'>
                 <div className='create-icon-1'></div>
